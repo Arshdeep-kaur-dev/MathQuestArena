823b0d999a4c8f238d22ae1f26bc83fd2cdb8a40
arshdeep <arshdeepkaurkahlon784@gmail.com>
2026-07-02 15:52:50 +0530

    Fix email port for Render

--- backend/mathquest_project/settings.py
+++ backend/mathquest_project/settings.py
@@ -124,8 +124,9 @@ GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_TIMEOUT = 10
