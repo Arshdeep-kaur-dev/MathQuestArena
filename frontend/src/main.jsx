839afee049149c522e0832a6dbd35769a51fcd3e
arshdeep <arshdeepkaurkahlon784@gmail.com>
2026-07-09 16:15:06 +0530

    placeholderr change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,9 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
+    <GoogleOAuthProvider
+      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "placeholder"}
+    >
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
